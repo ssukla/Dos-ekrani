8d074b96d0713993d386398bc3bed750a17ec0f5
ssukla <39220226+ssukla@users.noreply.github.com>
2021-01-26 07:54:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-$(document).ready(function () {
-
-	var loc = window.location.href+'';
-	if (loc.indexOf('http://')==0){
-		window.location.href = loc.replace('http://','https://');	// https redirect
-	}
-	
-	$('body').keypress(function(e) {
-	
-		if (stage == 1) {	//fent a kurzor
-			if(e.which == 13) { //enter
-				$('#formattingWrap').show();
-				timedCount();
-				stage = 2;
-				$('#typing').removeClass('typing');
-				
-			} else if(e.which == 8) { //backspace
-				//str = str.substring(0, str.length - 1);
-				
-				$('#typing').html($('#typing').html().substring(0, $('#typing').html().length - 1)); // cut off last character
-				
-				
-			} else if(e.which == 32) { //space
-				$('#typing').html($('#typing').html() + '_')
+var percent = 0;	//max 20
+var i;
+var stage = 1;
+function timedCount(){
+	percent++;
+    if (percent <= 20) {
+		var seg ="";
+		for (i = 0; i <= 20; i++) {
+			if (i <= percent) {
+				seg = seg + '&#9608;';
 			} else {
-				$('#typing').html($('#typing').html() + String.fromCharCode(e.which))
+				seg = seg + '&#9617;';
 			}
+			$('#formatting').html(seg);
 		}
+		$('#percent').html(percent*5-(Math.floor(Math.random() * 5) + 1) );
+		t = setTimeout("timedCount()",(300 + (Math.floor(Math.random() * 800) + 1)));
+	} else {
+		$('#percent').html('100');
+		$('#formatComplete').show();
+		stage = 3;
+	}
+}
