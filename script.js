32f094990a714ba72378491ac8f6057d39c7ea14
ssukla <39220226+ssukla@users.noreply.github.com>
2021-01-26 07:59:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-var percent = 0;	//max 20
-var i;
-var stage = 1;
-function timedCount(){
-	percent++;
-    if (percent <= 20) {
-		var seg ="";
-		for (i = 0; i <= 20; i++) {
-			if (i <= percent) {
-				seg = seg + '&#9608;';
-			} else {
-				seg = seg + '&#9617;';
-			}
-			$('#formatting').html(seg);
-		}
-		$('#percent').html(percent*5-(Math.floor(Math.random() * 5) + 1) );
-		t = setTimeout("timedCount()",(300 + (Math.floor(Math.random() * 800) + 1)));
-	} else {
-		$('#percent').html('100');
-		$('#formatComplete').show();
-		stage = 3;
+$(document).ready(function () {
+
+	var loc = window.location.href+'';
+	if (loc.indexOf('http://')==0){
+		window.location.href = loc.replace('http://','https://');	// https redirect
 	}
-}
