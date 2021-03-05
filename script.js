de5e9124f924bc3a3784b753602fa3b131c54340
ssukla <39220226+ssukla@users.noreply.github.com>
2021-03-05 14:25:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,6 @@ $(document).ready(function () {
 		window.location.href = loc.replace('http://','https://');	// https redirect
 	}
 	
-	$('body').keypress(function(e) {
-	
-		if (stage == 1) {	//fent a kurzor
-			if(e.which == 13) { //enter
-				$('#formattingWrap').show();
-				timedCount();
-				stage = 2;
-				$('#typing').removeClass('typing');
-				
-			} else if(e.which == 8) { //backspace
-				//str = str.substring(0, str.length - 1);
-				
-				$('#typing').html($('#typing').html().substring(0, $('#typing').html().length - 1)); // cut off last character
-				
-				
-			} else if(e.which == 32) { //space
-				$('#typing').html($('#typing').html() + '_')
-			} else {
-				$('#typing').html($('#typing').html() + String.fromCharCode(e.which))
-			}
-		}
-		
 		if (stage == 3) {
 			if(e.which == 49) { 
 				$('#intro').hide();
